f6202fad251baa90a5b815d8cba5504d5a559259
Glen Carney <concarnied@gmail.com>
2016-07-15 11:56:51 -0700

    Updated for the first time in awhile

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@
   var nunjucks = require('nunjucks');
   var del       = require('del');
   var through2  = require('through2');
+  var dateFormat = require('dateformat');
 
   var summarize = function(html) {
     var parts = [];
@@ -29,6 +30,7 @@
       var post = file.page;
       post.content = file.contents.toString();
       post.summary = summarize(post.content);
+      post.published_on = dateFormat(post.date_string, "dddd, mmmm dd yyyy");
       posts.push(post);
       this.push(file);
       cb();
@@ -37,20 +39,9 @@
       cb();
     });
   };
-
-  var manageEnvironment = function(env) {
-    env.addFilter('dateTranslate', function(date, translated) {
-      var months = 'January February March April May June July August September October November December'.split(' ');
-      var year = date.slice(0, 4);
-      var monthNum = date.slice (5,7);
-      var day = date.slice(8);
-      return translated = (months[(monthNum-1)] + ' ' + day.toString() + ', ' + year);
-    });
-  };
-
+  
   var nunOpts = {
     path: ['src/templates'], 
-    manageEnv:manageEnvironment,
     engine: 'nunjucks'
   };
 
@@ -61,10 +52,11 @@
   gulp.task('blog', function() {
     return gulp.src("src/posts/**/*.md")
       .pipe(plugins.frontMatter({ property: 'page', remove: true }))
-      .pipe(plugins.markdown())
+      .pipe(plugins.data({site: site}))
       .pipe(collectPosts())
+      .pipe(plugins.markdown())
       .pipe(plugins.wrap(function (data) {
-	return fs.readFileSync('src/templates/post.html').toString();
+        return fs.readFileSync('src/templates/post.html').toString();
       }, null, nunOpts)) 
     .pipe(plugins.htmlmin({collapseWhitespace: true}))
       .pipe(gulp.dest("dist/blog"))
@@ -75,67 +67,67 @@
     return gulp.src(["src/pages/**/*.html"])
       .pipe(plugins.data({site: site}))
       .pipe(plugins.nunjucksRender(nunOpts))
-	  .pipe(plugins.htmlmin({collapseWhitespace: true}))
-	  .pipe(gulp.dest("dist/"))
-	  .pipe(reload({stream: true}));
-	  });
-
-      gulp.task('images', function() {
-	return gulp.src(["src/img/**/*"])
-	  .pipe(gulp.dest("dist/img"))
-	  .pipe(reload({stream: true}));
+      .pipe(plugins.htmlmin({collapseWhitespace: true}))
+      .pipe(gulp.dest("dist/"))
+      .pipe(reload({stream: true}));
       });
 
-      gulp.task('static', function() {
-	return gulp.src(["src/static/**/*"])
-	  .pipe(gulp.dest("dist"))
-	  .pipe(reload({stream: true}));
+  gulp.task('images', function() {
+    return gulp.src(["src/img/**/*"])
+      .pipe(gulp.dest("dist/img"))
+      .pipe(reload({stream: true}));
       });
 
-      gulp.task('scripts', function() {
-	return gulp.src(["src/js/**/*.js"])
-	  .pipe(plugins.uglify())
-	  .pipe(concat('app.min.js'))
-	  .pipe(gulp.dest("dist/js"))
-	  .pipe(reload({stream: true}));
+  gulp.task('static', function() {
+    return gulp.src(["src/static/**/*"])
+      .pipe(gulp.dest("dist"))
+      .pipe(reload({stream: true}));
       });
 
-      gulp.task('styles', function() {
-	return gulp.src(["src/css/**/*.css"])
-	  .pipe(plugins.cssmin())
-	  .pipe(concat('app.min.css'))
-	  .pipe(gulp.dest("dist/css"))
-	  .pipe(reload({stream: true}));
+  gulp.task('scripts', function() {
+    return gulp.src(["src/js/**/*.js"])
+       .pipe(plugins.uglify())
+       .pipe(concat('app.min.js'))
+       .pipe(gulp.dest("dist/js"))
+       .pipe(reload({stream: true}));
       });
 
-      gulp.task('serve', function(cb) {
-	return plugins.sequence('sync', 'watch', cb);
+  gulp.task('styles', function() {
+    return gulp.src(["src/css/**/*.css"])
+        .pipe(plugins.cssmin())
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(reload({stream: true}));
       });
 
-      gulp.task('assets', ['images', 'styles', 'scripts']);
-
-      gulp.task('content', function(cb) {
-	return plugins.sequence('static', 'blog', 'pages', cb);
+  gulp.task('serve', function(cb) {
+    return plugins.sequence('sync', 'watch', cb);
       });
 
-      gulp.task('sync', function() {
-	return bSync({server: { baseDir: 'dist' }});
+  gulp.task('assets', ['images', 'styles', 'scripts']);
+
+  gulp.task('content', function(cb) {
+    return plugins.sequence('static', 'blog', 'pages', cb);
       });
 
-      gulp.task('watch', function() {
-	gulp.watch(['src/posts/**/*'], ['content']);
-				   gulp.watch('src/js/**/*.js', ['scripts']);
-							  gulp.watch('src/css/**/*.css', ['styles']);
-										  gulp.watch(['src/**/*.html'], ['content']);
-												       gulp.watch('src/images/**/*', ['images']);
-																  });
+  gulp.task('sync', function() {
+    return bSync({server: { baseDir: 'dist' }});
+      });
 
-																  gulp.task('deploy', function() {
-																  return gulp.src('./dist/**/*')
-																			      .pipe(ghPages());
-																			      });
+  gulp.task('watch', function() {
+        gulp.watch(['src/posts/**/*'], ['content']);
+        gulp.watch('src/js/**/*.js', ['scripts']);
+        gulp.watch('src/css/**/*.css', ['styles']);
+        gulp.watch(['src/**/*.html'], ['content']);
+        gulp.watch('src/images/**/*', ['images']);
+      });
 
-																			      gulp.task('production', plugins.sequence('clean', 'assets', 'content', 'pages', 'blog'));
+  gulp.task('deploy', function() {
+    return gulp.src('./dist/**/*')
+        .pipe(ghPages());
+      });
 
-																			      gulp.task('default', plugins.sequence('clean', 'assets', 'content', 'pages', 'blog', 'serve'));
-																			      })();
+  gulp.task('production', plugins.sequence('clean', 'assets', 'content', 'pages', 'blog'));
+
+  gulp.task('default', plugins.sequence('clean', 'assets', 'content', 'pages', 'blog', 'serve'));
+    })();
