0a5d002c06dade762631e3676d541c54f2637070
Glen Carney <concarnied@gmail.com>
2016-08-04 13:01:39 -0700

    Deleted comments

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,3 @@
-/*$(document).ready(function(){
-  var posts = [];
-  var mergeArticleJson = function(template, json){
-    return template; 
-  };
-  var insertTaggedPosts = function(){
-    var json = JSON.parse($('<div/>').html($('#json_posts').html()).text());
-    console.log(json);
-    var tmpl = $('#taggedPostTemplate').html();
-    console.log(tmpl);
-    for (var key in json){
-      posts.push(mergeArticleJson(tmpl, json[key]));
-    }
-  };
-  insertTaggedPosts();
-  console.log(insertTaggedPosts());
-});
-*/
-
 $(document).ready(function(){
   var jsonPosts = JSON.parse($('<div/>').html($('#json_posts').html()).text());
 
